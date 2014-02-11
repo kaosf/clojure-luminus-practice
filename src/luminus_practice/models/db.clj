bc7a3817db8088ab422be823015337bbe721b356
ka <ka.kaosf@gmail.com>
2014-02-11 16:31:10 +0900

    set up database connection
    ref. http://www.luminusweb.net/docs/database.md

--- src/luminus_practice/models/db.clj
+++ src/luminus_practice/models/db.clj
@@ -1,3 +1,8 @@
 (ns luminus-practice.models.db
   (:use korma.core
         [korma.db :only (defdb)]))
+
+(def db-spec {:subprotocol "postgresql"
+              :subname "//localhost/my_website"
+              :user "admin"
+              :password "admin"})
